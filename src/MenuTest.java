21171a636775f802886d21e46fbcfbfb5c3390cd
Vianney <vianney.demarquet@noos.fr>
2018-05-08 17:31:39 +0200

    rapport

---
+++ src/MenuTest.java
@@ -0,0 +1,299 @@
+import  javax.swing.*;
+import  java.awt.*;
+import  java.awt.event.*;
+import java.io.*;
+
+/**
+ * La classe <code>MenuTest</code> est utilis&eacute;e pour int&eacute;ragir avec l'utilisateur avec l'aide d'un menu; et ainsi g&eacute;r&eacute; les param&egrave;tres d'une nouvelle partie.
+ * Ou de continuer une partie sauvegarder au pr&eacute;alable
+ * @author Elric Launay
+ * @version 0.1
+ */
+public class MenuTest extends JFrame implements ActionListener, MouseListener{
+  private JFrame fenetre;
+  private JFrame choix;
+  private int nb_ligne = 4;
+  private int nb_colonne = 4;
+  private int nb_bombe= 1;
+  private JLabel ligne;
+  private JLabel colonne;
+  private JLabel bombe; 
+
+   	/**
+	* Affiche un menu contenant 3 boutons
+	* un bouton Nouvelle partie , Continuer la partie et Quitter 
+	*/
+  public MenuTest()
+  {
+    super("MenuTest");
+    fenetre = new JFrame();
+    GridLayout gestionnaire = new GridLayout(0,1);
+    gestionnaire.setHgap(35); 
+    gestionnaire.setVgap(35);   
+    boolean var=Continuer();
+    fenetre.setLayout(gestionnaire);
+    JButton b1 = new JButton("Nouvelle partie");
+    JButton b2 = new JButton("Continuer la partie");
+    JButton b3 = new JButton("Quitter");
+    b1.addActionListener(this);
+    if (var == false){
+      b2.setBackground(new Color(230, 230, 230));
+    }else
+    {
+      b2.addActionListener(this);
+    }
+
+    b3.addActionListener(this);
+    fenetre.add(b1);
+    fenetre.add(b2);
+    fenetre.add(b3);
+    fenetre.setSize(800, 800);
+    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    fenetre.setVisible(true);
+  }
+
+  	/**
+	* Affiche un menu contenant 13 boutons
+	* 3 boutons +, 3 boutons -,3 bouton max et 3 bouton min 
+	* Choix de la valeurs des 3 variables n&eacute;cessaires a la g&eacute;n&eacute;ration du d&eacute;mineur.
+	*
+	*/
+  private boolean Continuer()
+  {
+    try
+    {
+      DataInputStream flux = new DataInputStream(new FileInputStream(new File("foo.bin")));
+      try{
+        flux.close();
+      }catch(IOException exception)
+      {
+        return false;
+      }
+    }catch(FileNotFoundException exception)
+    {
+      return false;
+    }
+    return true;
+  }
+
+  public void nouvellePartie(){
+    choix = new JFrame();
+    GridLayout gestionnaire = new GridLayout(4,5);
+    choix.setLayout(gestionnaire);
+    ligne = new JLabel(nb_ligne+" ligne",JLabel.CENTER); 
+    colonne = new JLabel(nb_colonne+" colonne",JLabel.CENTER); 
+    bombe = new JLabel(nb_bombe+" bombe",JLabel.CENTER); 
+
+
+    JButton plus_colonne = new JButton("+");
+    JButton moins_colonne = new JButton("-");
+    plus_colonne.setName("plus_colonne");
+    moins_colonne.setName("moins_colonne");
+
+    JButton plus_ligne = new JButton("+");
+    JButton moins_ligne = new JButton("-");
+    plus_ligne.setName("plus_ligne");
+    moins_ligne.setName("moins_ligne");
+
+    JButton max_colonne = new JButton("Max");
+    JButton min_colonne = new JButton("Min");
+    max_colonne.setName("max_colonne");
+    min_colonne.setName("min_colonne");
+
+    JButton max_ligne = new JButton("Max");
+    JButton min_ligne = new JButton("Min");
+    max_ligne.setName("max_ligne");
+    min_ligne.setName("min_ligne");
+
+    JButton plus_bombe = new JButton("+");
+    JButton moins_bombe = new JButton("-");
+    plus_bombe.setName("plus_bombe");
+    moins_bombe.setName("moins_bombe");
+
+    JButton max_bombe = new JButton("Max");
+    JButton min_bombe = new JButton("Min");
+    max_bombe.setName("max_bombe");
+    min_bombe.setName("min_bombe");
+
+    JButton valider = new JButton("Valider");
+    valider.setName("Valider");
+    valider.addMouseListener(this);
+    JLabel l1 = new  JLabel("");
+    JLabel l2 = new  JLabel("");
+    JLabel l3 = new  JLabel("");
+    JLabel l4 = new  JLabel("");
+
+    
+
+    plus_colonne.addMouseListener(this);
+
+
+    moins_colonne.addMouseListener(this);
+
+
+
+    plus_ligne.addMouseListener(this);
+    
+
+    moins_ligne.addMouseListener(this);
+    
+
+    min_colonne.addMouseListener(this);
+    max_colonne.addMouseListener(this);
+
+    min_ligne.addMouseListener(this);
+    max_ligne.addMouseListener(this);
+
+    min_bombe.addMouseListener(this);
+    max_bombe.addMouseListener(this);
+    plus_bombe.addMouseListener(this);
+    moins_bombe.addMouseListener(this);
+
+    choix.add(min_colonne);
+    choix.add(moins_colonne);
+    choix.add(colonne);
+    choix.add(plus_colonne);
+    choix.add(max_colonne);
+
+    choix.add(min_ligne);
+    choix.add(moins_ligne);
+    choix.add(ligne);
+    choix.add(plus_ligne);
+    choix.add(max_ligne);
+
+    choix.add(min_bombe);
+    choix.add(moins_bombe);
+    choix.add(bombe);
+    choix.add(plus_bombe);
+    choix.add(max_bombe);
+
+    choix.add(l1);
+    choix.add(l2);
+    choix.add(valider);
+    choix.add(l3);
+    choix.add(l4);
+
+    choix.setSize(800, 800);
+    choix.setVisible(true);
+    choix.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  	/**
+	* Permet de tester si les valeurs des variables sont comprises dans les intervalles demand&eacute;s.
+	* Affiche la valeur choisit par l'utilisateur sur la frame de choix avant validation.
+	* 
+	*/
+  private void rename(){
+    if(nb_ligne>30){
+      nb_ligne=30;
+    }else if(nb_ligne<4){
+      nb_ligne=4;
+    }else if(nb_colonne>30){
+      nb_colonne=30;
+    }else if(nb_colonne<4){
+      nb_colonne=4;
+    }else if(nb_bombe>(nb_ligne*nb_colonne)/3){
+      nb_bombe=(nb_ligne*nb_colonne)/3;
+    }else if(nb_bombe<1){
+      nb_bombe=1;
+    }
+    ligne.setText(nb_ligne+" ligne");
+    bombe.setText(nb_bombe+" bombe");
+    colonne.setText(nb_colonne+" colonne");
+  }
+
+	/**
+	* Efectue l'action sp&eacute;cifiqe au bouton cliqu&eacute; dans le menu.
+	*
+	* @param e case sur la quelle on a cliqu&eacute;
+	*/
+  public void actionPerformed(ActionEvent e)
+  {
+    if(e.getActionCommand()=="Nouvelle partie")
+    {
+      fenetre.dispose();
+      nouvellePartie();
+    }
+    else if(e.getActionCommand()=="Continuer la partie")
+    {
+      fenetre.dispose();
+      new Partie();
+    }
+    else if(e.getActionCommand()=="Quitter")
+    {
+      System.exit(0);
+    }
+
+  }
+
+  /**
+  *Ne fais rien.
+  *
+  */
+  public void mouseClicked(MouseEvent e){
+  }     // un bouton cliqué
+
+  /**
+  *Ne fais rien.
+  *
+  */
+  public void mouseEntered(MouseEvent e){
+  }     // debut du survol
+
+  /**
+  *Ne fais rien.
+  *
+  */
+  public void mouseExited(MouseEvent e){
+  }     // fin du survol
+
+  /**
+  *Ne fais rien.
+  *
+  */
+  public void mousePressed(MouseEvent e){
+  }     // un bouton appuyé
+
+  /**
+  * Modifie les valeurs de nb_ligne nb_colonne et nb_bombe
+  * si on cliquegauche sur l'un des boutons nomm&eacute;s
+  *	on incr&eacute;mente ou d&eacute;cremente les valeurs des variables 
+  *
+  * @param e bouton sur lequel on a cliqu&eacute;
+  */
+  public void mouseReleased(MouseEvent e)
+  {
+    String x=e.getComponent().getName();
+    if (x=="min_ligne") {
+      nb_ligne=4;      
+    }else if(x=="max_ligne"){
+      nb_ligne=30;
+    }else if(x=="moins_ligne"){
+      nb_ligne--;
+    }else if(x=="plus_ligne"){
+      nb_ligne++;
+    }else if (x=="min_colonne") {
+      nb_colonne=4;      
+    }else if(x=="max_colonne"){
+      nb_colonne=30;
+    }else if(x=="moins_colonne"){
+      nb_colonne--;
+    }else if(x=="plus_colonne"){
+      nb_colonne++;
+    }else if (x=="min_bombe") {
+      nb_bombe=1;      
+    }else if(x=="max_bombe"){
+      nb_bombe=(nb_ligne*nb_colonne)/3;
+    }else if(x=="moins_bombe"){
+      nb_bombe--;
+    }else if(x=="plus_bombe"){
+      nb_bombe++;
+    }else if (x=="Valider") {
+      choix.dispose();
+      new Partie(nb_ligne,nb_colonne,nb_bombe);
+    }
+    rename();
+  }
+
+}
+
---
+++ src/Partie.java
@@ -0,0 +1,365 @@
+import javax.swing.*;
+import java.awt.*;
+import java.lang.Math;
+import java.lang.Object;
+import  java.awt.event.*;
+import java.io.*;
+
+/**
+ * La classe <code>Partie</code> est utilis&eacute;e pour comenc&eacute; et g&eacute;r&eacute; une partie en cour.
+ *  
+ * @author Vianney Demarquet
+ * @version 0.1
+ */
+public class Partie {
+	
+	private int[][] grille_bombe;
+	private int[][] grille_partie;
+	private int lignes;
+	private int colonnes;
+	private int nbbombes;
+	private int case_libre;
+	private int drapeaux;
+
+	private Affichage affiche;
+
+	/**
+	* Constructeur destin&eacute; &agrave; la cr&eacute;ation de certaine
+	* constantes publiques.
+	*
+	* @param l nombre de lignes souhait&eacute; (entre 4 et 30)
+	* @param c nombre de colonnes souhait&eacute; (entre 4 et 30)
+	* @param b nombre de bombe souhait&eacute;
+	*/
+
+	public Partie(int l, int c, int b) {
+		grille_bombe = new int[l][c];
+		grille_partie = new int[l][c];
+		lignes=l;
+		colonnes=c;
+		nbbombes=b;
+		case_libre=(lignes*colonnes)-nbbombes;
+		drapeaux=0;
+		this.Zéro();
+		this.Place_Bomb();
+		affiche = new Affichage(this);
+		
+	}
+
+	/**
+	* Constructeur destin&eacute; &agrave; la cr&eacute;ation de certaine 
+	* constantes publiques à partire d'une sauvegarde.
+	*/
+	public Partie()
+	{
+		new Load(this);
+		affiche = new Affichage(this);
+	}
+
+	/*
+	zéroiphie les valeur du tableau;
+	*/
+	private void Zéro(){
+		for (int i=0; i<lignes; i++) {
+			for (int y=0; y<colonnes; y++) {
+				grille_bombe[i][y]=0;
+				grille_partie[i][y]=0;
+			}
+		}
+	}
+
+	/**
+	* Renvoie le nombre de lignes.
+	*
+	* @return le nombre de lignes (entre 4 et 30)
+	*/
+	public int getLignes()
+	{
+		return lignes;
+	}
+
+	/**
+	* Renvoie le nombre de colonnes.
+	*
+	* @return le nombre de colonnes (entre 4 et 30)
+	*/
+	public int getColonnes()
+	{
+		return colonnes;
+	}
+
+	/**
+	* Renvoie le nombre de bombes.
+	*
+	* @return le nombre de bombes
+	*/
+	public int getNbbombes()
+	{
+		return nbbombes;
+	}
+
+	/**
+	* Renvoie le nombre de bombes autour de cette case
+	*
+	* @return le nombre de bombes
+	* @param l le num&eacute;ro ligne de la case
+	* @param c le num&eacute;ro colonnes de la case
+	*/
+	public int getBomb(int l, int c)
+	{
+		return grille_bombe[l][c];
+	}
+
+	/**
+	* Renvoie renvoie si la case et cach&eacute;, r&eacute;v&eacute;l&eacute;
+	* ou avec un marqueur.
+	*
+	* @return &eacute;tat de la case
+	* @param l le num&eacute;ro ligne de la case
+	* @param c le num&eacute;ro colonnes de la case
+	*/
+	public int getPartie(int l, int c)
+	{
+		return grille_partie[l][c];
+	}
+
+	/**
+	* Renvoie le nombre de case libre.
+	*
+	* @return le nombre de case libre
+	*/
+	public int getCase_libre()
+	{
+		return case_libre;
+	}
+
+	/**
+	* Renvoie le nombre de drapeaux.
+	*
+	* @return le nombre de drapeaux
+	*/
+	public int getDrapeaux()
+	{
+		return drapeaux;
+	}
+
+	/**
+	* Initilise le nombre de lignes.
+	*
+	* @param l le nombre de lignes
+	*/
+	public void setLignes(int l)
+	{
+		lignes=l;
+	}
+
+	/**
+	* Initilise le nombre de colonnes.
+	*
+	* @param c le nombre de colonnes
+	*/
+	public void setColonnes(int c)
+	{
+		colonnes=c;
+	}
+
+	/**
+	* Initilise la taille des tableaux.
+	*
+	*/
+	public void setTables()
+	{
+		grille_bombe = new int[lignes][colonnes];
+		grille_partie = new int[lignes][colonnes];
+	}
+
+	/**
+	* Change la valeur de la case de cach&eacute; &agrave; r&eacute;v&eacute;l&eacute;.
+	*
+	* @param l le num&eacute;ro de lignes
+	* @param c le num&eacute;ro de colonnes
+	*/
+	public void setPartie(int l, int c)
+	{
+		grille_partie[l][c]=1;
+	}
+
+	/**
+	* Change la valeur de la case de cach&eacute;, r&eacute;v&eacute;l&eacute;
+	* ou avec un marqueur.
+	*
+	* @param l le num&eacute;ro de lignes
+	* @param c le num&eacute;ro de colonnes
+	* @param val &eacute;tat de la case
+	*/
+	public void setPartie(int l, int c,int val)
+	{
+		grille_partie[l][c]=val;
+	}
+
+	/**
+	* Change le nombre de bombe des case alentour.
+	*
+	* @param l le num&eacute;ro de lignes
+	* @param c le num&eacute;ro de colonnes
+	* @param val le nombre de bombe souhait&eacute;
+	*/
+	public void setBombs(int l, int c,int val)
+	{
+		grille_bombe[l][c]=val;
+	}
+
+	/**
+	* Change le nombre de case libre
+	*
+	* @param val le nombre de case libre
+	*/
+	public void setCase_libre(int val)
+	{
+		case_libre=val;
+	}
+
+	/**
+	* change le nombre de bombes
+	*
+	* @param val le nombre de bombes
+	*/
+	public void setNbbombes(int val)
+	{
+		nbbombes=val;
+	}
+
+	/**
+	* Change le nombre de drapeaux
+	*
+	* @param val le nombre de drapeaux
+	*/
+	public void setDrapeaux(int val)
+	{
+		drapeaux=val;
+	}
+
+		
+	/*
+	place les bombes dans la grille_bombe
+	*/
+	private void Place_Bomb(){
+		double y;
+		int l,c;
+		java.util.Random x = new java.util.Random();
+		for (int i=0; i<nbbombes; i++) {
+			y = x.nextDouble(); //génération du nombre aléatoire
+			y = y*lignes;
+			l = (int)y;
+			y = x.nextDouble();
+			y = y*colonnes;
+			c = (int)y;
+
+			if (grille_bombe[l][c] == 9)
+			{
+				i--;
+			}else
+			{
+				grille_bombe[l][c] = 9; //code des bombes
+				for (int k=-1;k<2; k++) 
+				{
+					if ((l+k)>=0 && (l+k)<lignes) 
+					{
+						for (int m=-1;m<2; m++) 
+						{
+							if ((c+m)>=0 && (c+m)<colonnes) 
+							{
+								if (grille_bombe[l+k][c+m]!=9) 
+								{
+									grille_bombe[l+k][c+m]++;		//incrémente le nombre de bombe au alentoure
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	private void Afficher_consol(){
+		System.out.println("nbcolonnes : "+colonnes);
+		System.out.println("nblignes : "+lignes);
+		System.out.println("case libre : "+case_libre);
+		
+		System.out.println("");
+		for (int i=0; i<lignes;i++) {
+			for (int k=0;k<colonnes ;k++ ) {
+				System.out.print(grille_bombe[i][k]+" ");
+			}
+			System.out.println("");
+		}
+		System.out.println("\n");
+		for (int i=0; i<lignes;i++) {
+			for (int k=0;k<colonnes ;k++ ) {
+				System.out.print(grille_partie[i][k]+" ");
+			}
+			System.out.println("");
+		}
+	}
+	
+
+	/**
+	* V&eacute;rifie si on &agrave; gagn&eacute; ou perdu.
+	*
+	* @param l le num&eacute;ro de lignes
+	* @param c le num&eacute;ro de colonnes
+	*/
+	public void Cont(int l, int c)
+	{
+		if (grille_bombe[l][c]==9) 
+		{
+			grille_partie[l][c]=4;
+			this.Perdu();
+			affiche.setFin();
+			affiche.MenuFin(true);
+		}else
+		{
+			case_libre--;
+			if (case_libre == 0) 
+			{
+				affiche.setFin();
+				affiche.MenuFin(false);
+			}
+		}
+		
+	}
+
+
+	private void Perdu()
+	{
+		for (int i=0; i<lignes; i++) {
+			for (int k=0; k<colonnes; k++) {
+				if (grille_partie[i][k]!=4 && grille_bombe[i][k]==9 && grille_partie[i][k]!=2) {
+					grille_partie[i][k]=1;
+				}else if (grille_partie[i][k]==2 && grille_bombe[i][k]!=9) {
+					grille_partie[i][k]=5;
+				}
+			}
+		}
+	}
+
+	public void Drapeau(int l, int c)
+	{
+		if (grille_partie[l][c]==0) {
+			grille_partie[l][c]=2;
+			drapeaux++;
+		}else if (grille_partie[l][c]==2) {
+			grille_partie[l][c]=3;
+			drapeaux--;
+		}else
+		{
+			grille_partie[l][c]=0;
+		}
+		affiche.Menu(nbbombes-drapeaux);
+	}
+
+
+}
+
+	
